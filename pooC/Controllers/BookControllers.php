1b8ea65c79aaa7c601f486cfe4abdb2d4beb088e
Mickael LY <ly.mickael@gmail.com>
2020-09-07 16:15:48 +0200

    connect to bdd

--- pooC/Controllers/BookControllers.php
+++ pooC/Controllers/BookControllers.php
@@ -1,9 +1,20 @@
 <?php
-
+require_once './Models/Book.php';
 class BookControllers
 {
   public function add()
   {
     var_dump('My role is to add some books in mysql');
+
+    require_once './Views/addBook.html';
+
+    $book = new Book();
+    if (isset($_POST['submit'])) {
+      $title = $book->setTitle($_POST['title']);
+      $author = $book->setAuthor($_POST['author']);
+      $resume = $book->setResume($_POST['resume']);
+
+      $book->insert($title, $author, $resume);
+    }
   }
 }
--- pooC/Models/Book.php
+++ pooC/Models/Book.php
@@ -1,60 +1,65 @@
 <?php
+class Book
+{
+  private $id;
+  private $title;
+  private $author;
+  private $resume;
 
-private $id;
-private $title;
-private $author;
-private $resume;
-
-public function getId() 
-{
-  return $this->id;
-}
-
-public function getTitle() 
-{
-  return $this->title;
-}
-public function setTitle($title) 
-{
-  return $this->title = $title;
-}
-
-public function getAuthor() 
-{
-  return $this->autor;
-}
-public function setAuthor($author) 
-{
-  return $this->author = $author;
-}
-
-public function getResume() 
-{
-  return $this->resume;
-}
-public function setResume($resume) 
-{
-  return $this->resume = $resume;
-}
-
-public function getConnection()
-{
-  try{
-    $db = new PDO('mysql:host=localhost;dbname=biblio', 
-    "afpaphp", "afpaphp");
-
-  } catch (PDOException $e){
-    print "Erreur";
+  public function getId()
+  {
+    return $this->id;
   }
-}
 
-public function insert($title, $author, $resume)
-{
-  $bdd = $this->getConnection();
-  $sql = $bdd->prepare("INSERT INTO book (title, author, resume) VALUES ('$title', '$author', '$resume')");
-
-  if (!$sql->execute()) {
-    die("Not WOrking Bro!");
+  public function getTitle()
+  {
+    return $this->title;
   }
-  header("Location: index.php");
+  public function setTitle($title)
+  {
+    return $this->title = $title;
+  }
+
+  public function getAuthor()
+  {
+    return $this->author;
+  }
+  public function setAuthor($author)
+  {
+    return $this->author = $author;
+  }
+
+  public function getResume()
+  {
+    return $this->resume;
+  }
+  public function setResume($resume)
+  {
+    return $this->resume = $resume;
+  }
+
+  public function getConnection()
+  {
+    try {
+      $bdd = new PDO(
+        'mysql:host=localhost;dbname=biblio',
+        'afpaphp',
+        'afpaphp'
+      );
+    } catch (PDOException $e) {
+      print "Erreur";
+    }
+    return $bdd;
+  }
+
+  public function insert($title, $author, $resume)
+  {
+    $bdd = $this->getConnection();
+    $sql = $bdd->prepare("INSERT INTO book (title, author, resume) VALUES ('$title', '$author', '$resume')");
+
+    if (!$sql->execute()) {
+      die("Not WOrking Bro!");
+    }
+    header("Location: index.php");
+  }
 }

b0163d9c805aaa3fd209081d22259360498361a5
Mickael LY <ly.mickael@gmail.com>
2020-09-07 11:14:20 +0200

    add new function

--- index1.php
+++ index1.php
@@ -18,3 +18,21 @@ function soustraction($nombre1, $nombre2)
 echo "<br>";
 echo soustraction(20, 2);
 echo "<br>";
+
+function multiplication($nombre1, $nombre2)
+{
+  $result = $nombre1 * $nombre2;
+  return $result;
+}
+echo "<br>";
+echo multiplication(12, 5);
+echo "<br>";
+
+function modulo($nombre1, $nombre2)
+{
+  $result = $nombre1 % $nombre2;
+  return $result;
+}
+echo "<br>";
+echo modulo(10, 3);
+echo "<br>";
